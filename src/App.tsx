2b7ad0450bc404ce2efcb96d94be91463a885da8
Leonardo Costa <leoalves0698@gmail.com>
2026-06-06 16:35:31 -0300

    feat: add certificates page in route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { Projects } from './pages/Projects';
 import { DefaultLayout } from './component/DefaultLayout';
+import { CertificadoPages } from './pages/CertificadosPages';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: '/projects',
         element: <Projects />,
+      },
+      {
+        path: '/certificates',
+        element: <CertificadoPages />,
       },
     ],
   },
